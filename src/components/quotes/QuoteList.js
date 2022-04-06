90c2c6389232ac23e143046b5b1d1e536387e820
Ruben Martinez <therubenm@gmail.com>
2022-04-06 02:05:03 -0500

    - Add and implement sorting logic for AllQuotes page - Base the sort on current query parameters - Conditionally show comments based on exactly which URL the user is on.

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,13 +1,43 @@
 import { Fragment } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 
 import QuoteItem from "./QuoteItem";
 import classes from "./QuoteList.module.css";
 
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
 const QuoteList = ({ quotes }) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
+
+  const isSortingAscending = queryParams.get("sort") === "asc";
+
+  const sortedQuotes = sortQuotes(quotes, isSortingAscending);
+
+  const handleChangeSorting = () => {
+    history.push("/quotes?sort=" + (isSortingAscending ? "desc" : "asc"));
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={handleChangeSorting}>
+          {/*If it's already sorting ascending, option available should be Sort Descending (and vice versa)*/}
+          Sort {isSortingAscending ? "Descending" : "Ascending"}
+        </button>
+      </div>
       <ul className={classes.list}>
-        {quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
